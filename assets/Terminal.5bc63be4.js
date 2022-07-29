3adacbcc89de86d1d7e7f47699f1fe9c15f2b670
Andrew Lucas <andrewlucasgs@gmail.com>
2022-07-29 17:58:43 -0300

    adding dist subtree

---
+++ assets/Terminal.5bc63be4.js
@@ -0,0 +1,2 @@
+import{e as o,f as a,j as s,k as r}from"./index.aa897cf5.js";const l={class:"h-full w-full bg-[#282a36] overflow-auto text-white p-2"},n={props:{output:{type:Array,default:[]}},setup(t){const e=t;return(p,u)=>(o(),a("div",l,[s("pre",null,">>> "+r(e.output.join(`
+`)),1)]))}};export{n as default};
--- assets/Terminal.aaa29f7d.js
+++
@@ -1,2 +0,0 @@
-import{f as o,h as a,A as s,C as r}from"./index.5d4a62fb.js";const l={class:"h-full w-full bg-[#282a36] overflow-auto text-white p-2"},n={props:{output:{type:Array,default:[]}},setup(t){const e=t;return(p,u)=>(o(),a("div",l,[s("pre",null,">>> "+r(e.output.join(`
-`)),1)]))}};export{n as default};
